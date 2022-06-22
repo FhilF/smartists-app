cbb33790f6be7994ce7acd5d43da59bf7a543157
FhilF <52390799+FhilF@users.noreply.github.com>
2022-06-22 15:36:19 +0800

    my changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   getSmartistsUserAsync,
 } from "utils/redux/slice/userSessionSlice";
 import { isMainnet } from "config";
+import { getPublicKeyFromPrivate,publicKeyToAddress } from "@stacks/encryption";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -126,6 +127,10 @@ const Content = (props) => {
     if (userSession) {
       if (userSession.isUserSignedIn()) {
         const userData = userSession.loadUserData();
+        console.log(userData)
+        const publicKey = getPublicKeyFromPrivate(userData.appPrivateKey)
+        console.log(publicKey)
+        console.log(publicKeyToAddress(publicKey))
         const walletAddressMainnet = userData.profile.stxAddress.mainnet;
         const walletAddressTestnet = userData.profile.stxAddress.testnet;
         const userSessionStorage = sessionStorage.getItem("SmartistsUser");
